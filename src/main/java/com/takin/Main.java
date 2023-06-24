0ed644c1601c7735b87b20c41977df715c4d068a
iTakinn <137518812+iTakinn@users.noreply.github.com>
2023-06-24 11:25:59 -0300

    começando jframe
    =D

--- src/main/java/com/takin/Main.java
+++ src/main/java/com/takin/Main.java
@@ -1,100 +1,89 @@
 package com.takin;
 
-import java.util.Scanner;
-import java.lang.Thread;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JLabel;
 import com.takin.core.criaruser;
 import com.takin.core.listardb;
 
-
 public class Main {
-    public static void main(String[] args)  {
-        Scanner scn = new Scanner(System.in);
-        System.out.println("escolha o modo de entrada:");
-        System.out.println("0 - usuario");
-        System.out.println("1 - administrador");
-        System.out.println();
-        int modo = scn.nextInt();
-    
-        if (modo == 1){
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Modo de Entrada");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        String[] options = { "Usuário", "Administrador" };
+        int modo = JOptionPane.showOptionDialog(frame, "Escolha o modo de entrada:", "Modo de Entrada",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+
+        if (modo == 1) {
             try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-            System.out.println("digite a senha pra isso...");
-            
-            int senhadita = scn.nextInt();
-            if (senhadita == 1234){
-                System.out.println();
-                System.out.println("Autorizado!");
-                System.out.println();
-                try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-                System.out.println("escolha uma merda:");
-                System.out.println("listar db - 1");
-                System.out.println("add um merda - 2");
-                System.out.println("tirar um merda - 3");
-                System.out.println("criar uma db nova - 4");
-                System.out.print("");
-                int numer = scn.nextInt();
-
-                if (numer == 1){
-                    System.out.println("Listando db...");
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-                    listardb.todaDB();
-                }  else if(numer == 2 ){
-                    System.out.println("criando um user...");
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-                    System.out.println("nome para o usuario:");
-                    scn.nextLine();
-                    String nomee = scn.nextLine();
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-                    System.out.println("");
-                    System.out.println("Digite a senha para o usuario:");
-                    System.out.print("");
-                    String senha = scn.nextLine();
-                    try {
-                        Thread.sleep(1500);
-                    } catch (InterruptedException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-                    criaruser.main(nomee, senha);
-
-                } else if( numer == 3){
-
-                } else if( numer == 4){
-
-                } else {
-
-                }
-            }else{
-                System.out.println("senha errada, tente novamente");
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        }else if (modo == 0){
-            System.out.println("entrando como usuario");
-            //açao usuario
+
+            String senhaDigitada = JOptionPane.showInputDialog(frame, "Digite a senha para continuar...");
+
+            if (senhaDigitada.equals("1234")) {
+                JOptionPane.showMessageDialog(frame, "Autorizado!", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                String[] adminOptions = { "Listar banco de dados", "Adicionar um usuário", "Remover um usuário",
+                        "Criar um novo banco de dados" };
+
+                int numer = JOptionPane.showOptionDialog(frame, "Escolha uma opção:", "Opções do Administrador",
+                        JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, adminOptions, adminOptions[0]);
+
+                switch (numer) {
+                    case 0:
+                        JOptionPane.showMessageDialog(frame, "Listando banco de dados...", "Ação Executada",
+                                JOptionPane.INFORMATION_MESSAGE);
+                        listardb.todaDB();
+                        break;
+
+                    case 1:
+                        String nome = JOptionPane.showInputDialog(frame, "Nome para o usuário:");
+                        String senha = JOptionPane.showInputDialog(frame, "Digite a senha para o usuário:");
+
+                        criaruser.main(nome, senha);
+                        break;
+
+                    case 2:
+                        // TODO: Implement removing a user
+                        break;
+
+                    case 3:
+                        // TODO: Implement creating a new database
+                        break;
+
+                    default:
+                        // Handle invalid option
+                        break;
+                }
+            } else {
+                JOptionPane.showMessageDialog(frame, "Senha incorreta. Tente novamente.", "Erro",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+        } else if (modo == 0) {
+            JOptionPane.showMessageDialog(frame, "Entrando como usuário", "Modo de Entrada",
+                    JOptionPane.INFORMATION_MESSAGE);
+            // Action for the user mode
+        } else {
+            JOptionPane.showMessageDialog(frame, "Modo inválido. Encerrando aplicação.", "Erro",
+                    JOptionPane.ERROR_MESSAGE);
         }
+
+        frame.dispose();
         
+        // Create and display the main JFrame
+        JFrame mainFrame = new JFrame("Main Frame");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.getContentPane().add(new JLabel("Welcome to the application!"));
+        mainFrame.pack();
+        mainFrame.setVisible(true);
     }
 }
