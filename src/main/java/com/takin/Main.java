fd6f121a7540d43f6d55d54b0d7a7992d67b26b1
iTakinn <137518812+iTakinn@users.noreply.github.com>
2023-06-24 11:14:07 -0300

    adicionado delayzinho

--- src/main/java/com/takin/Main.java
+++ src/main/java/com/takin/Main.java
@@ -1,8 +1,7 @@
 package com.takin;
 
-import java.sql.Time;
 import java.util.Scanner;
-
+import java.lang.Thread;
 import com.takin.core.criaruser;
 import com.takin.core.listardb;
 
@@ -17,9 +16,25 @@ public class Main {
         int modo = scn.nextInt();
     
         if (modo == 1){
+            try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
             System.out.println("digite a senha pra isso...");
+            
             int senhadita = scn.nextInt();
             if (senhadita == 1234){
+                System.out.println();
+                System.out.println("Autorizado!");
+                System.out.println();
+                try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
                 System.out.println("escolha uma merda:");
                 System.out.println("listar db - 1");
                 System.out.println("add um merda - 2");
@@ -30,17 +45,40 @@ public class Main {
 
                 if (numer == 1){
                     System.out.println("Listando db...");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
                     listardb.todaDB();
                 }  else if(numer == 2 ){
                     System.out.println("criando um user...");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
                     System.out.println("nome para o usuario:");
                     scn.nextLine();
                     String nomee = scn.nextLine();
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
                     System.out.println("");
                     System.out.println("Digite a senha para o usuario:");
                     System.out.print("");
                     String senha = scn.nextLine();
-                    
+                    try {
+                        Thread.sleep(1500);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
                     criaruser.main(nomee, senha);
 
                 } else if( numer == 3){
